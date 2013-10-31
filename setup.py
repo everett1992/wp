bdd63808b4af9fb0b62aef9d8fdfa0122b1d28a0
Nicholas Vitovitch <nvitovitch@theapiary.co>
2013-10-30 21:54:52 -0400

    Templates arg parser, helper functions, misc other thing

---
+++ setup.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='wp',
+      version='0.1.0',
+      description='Wallpaper Manager',
+      package_dir={'', 'lib'},
+      scripts=['scripts/wp']
+     )
